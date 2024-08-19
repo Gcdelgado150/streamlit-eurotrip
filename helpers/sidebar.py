aefcb6d3b4308b91d946c35a17467afb9b3cde5b
Gcdelgado150 <gcdelgado15@gmail.com>
2024-08-19 19:52:32 -0300

    visao geral

--- helpers/sidebar.py
+++ helpers/sidebar.py
@@ -6,7 +6,7 @@ import os
 def create_sidebar():
     # Clear the sidebar
     st.sidebar.title("Eurotrip")
-    st.sidebar.page_link("home.py", label="Inicio") 
+    st.sidebar.page_link("home.py", label="Visão Geral") 
     st.sidebar.page_link("pages/rome.py", label="Roma")
     st.sidebar.page_link("pages/florence.py", label="Florença")
     st.sidebar.page_link("pages/venezia.py", label="Veneza")
